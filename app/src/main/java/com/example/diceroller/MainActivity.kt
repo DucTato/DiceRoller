73c38b4e53f50b138d90663b5d2053a36cff05ca
DucTato <65166409+DucTato@users.noreply.github.com>
2023-10-16 15:36:40 +0700

    Update 1
    Added minor animation to the dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.diceroller
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -29,12 +32,13 @@ class MainActivity : AppCompatActivity() {
          * This part is for updating the textView component
          **/
         val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRollResult.toString()
+
         /**
          * This part is for updating the imageView component
          **/
         val resultImageView: ImageView = findViewById(R.id.imageView)
-//        when (diceRollResult) {
+
+        //        when (diceRollResult) {
 //            1 -> resultImageView.setImageResource(R.drawable.dice_1)
 //            2 -> resultImageView.setImageResource(R.drawable.dice_2)
 //            3 -> resultImageView.setImageResource(R.drawable.dice_3)
@@ -42,17 +46,37 @@ class MainActivity : AppCompatActivity() {
 //            5 -> resultImageView.setImageResource(R.drawable.dice_5)
 //            6 -> resultImageView.setImageResource(R.drawable.dice_6)
 //        }
-        val diceFace = when (diceRollResult) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            6 -> R.drawable.dice_6
-            else -> R.drawable.dice_6
+        fun setDiceFace(face: Int) {
+            val diceFace = when (face) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                6 -> R.drawable.dice_6
+                else -> R.drawable.dice_6
+            }
+            resultImageView.setImageResource(diceFace)
         }
-        resultImageView.setImageResource(diceFace)
-        resultImageView.contentDescription = diceRollResult.toString()
+
+        @SuppressLint("SetTextI18n")
+        fun rollAnimation() {
+            val rollButton : Button = findViewById(R.id.button)
+            rollButton.visibility = View.INVISIBLE
+            resultTextView.text = getString(R.string.rolling_message)
+            val countDown = object : CountDownTimer(3000, 250) {
+                override fun onTick(p0: Long) {
+                    setDiceFace((1..6).random())
+                }
+                override fun onFinish() {
+                    setDiceFace(diceRollResult)
+                    resultTextView.text = diceRollResult.toString()
+                    resultImageView.contentDescription = diceRollResult.toString()
+                    rollButton.visibility = View.VISIBLE
+                }
+            }.start()
+        }
+        rollAnimation()
     }
 }
 
