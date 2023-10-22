f25e598df188bc623ba07d3ed9e6642b027c65ab
DucTato <65166409+DucTato@users.noreply.github.com>
2023-10-22 17:00:57 +0700

    Update 3.1
    Completed function for multiple dices. Access multiple dices by rotating the device sideway

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.diceroller
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.diceroller.databinding.ActivityMainBinding
@@ -22,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val diceToRoll = Dice(6)
-        val diceRollResult = diceToRoll.roll()
+        val diceRollResults = arrayOf( diceToRoll.roll(), diceToRoll.roll(), diceToRoll.roll(), diceToRoll.roll())
 
         /**
          * This part is for updating the textView component
@@ -32,39 +30,102 @@ class MainActivity : AppCompatActivity() {
         /**
          * This part is for updating the imageView component
          **/
-        val resultImageView: ImageView = findViewById(R.id.redDiceView)
 
-        fun setDiceFace(face: Int) {
-            val diceFace = when (face) {
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                6 -> R.drawable.dice_6
-                else -> R.drawable.dice_6
+
+        fun setDiceFace(face: Int, diceType: Int) {
+            val diceFace: Int
+            when (diceType){
+                /* 0 = Red */
+                0 -> {
+                    diceFace = when (face)
+                    {
+                        1 -> R.drawable.dice_1
+                        2 -> R.drawable.dice_2
+                        3 -> R.drawable.dice_3
+                        4 -> R.drawable.dice_4
+                        5 -> R.drawable.dice_5
+                        6 -> R.drawable.dice_6
+                        else -> R.drawable.dice_6
+                    }
+                    activityBinding.redDiceView.setImageResource(diceFace)
+                }
+                /* 1 = Blue */
+                1 -> {
+                    diceFace = when (face)
+                    {
+                        1 -> R.drawable.bdice_1
+                        2 -> R.drawable.bdice_2
+                        3 -> R.drawable.bdice_3
+                        4 -> R.drawable.bdice_4
+                        5 -> R.drawable.bdice_5
+                        6 -> R.drawable.bdice_6
+                        else -> R.drawable.bdice_6
+                    }
+                    activityBinding.blueDiceView?.setImageResource(diceFace)
+                }
+                /* 2 = Purple */
+                2 -> {
+                    diceFace = when (face)
+                    {
+                        1 -> R.drawable.pdice_1
+                        2 -> R.drawable.pdice_2
+                        3 -> R.drawable.pdice_3
+                        4 -> R.drawable.pdice_4
+                        5 -> R.drawable.pdice_5
+                        6 -> R.drawable.pdice_6
+                        else -> R.drawable.pdice_6
+                    }
+                    activityBinding.purpleDiceView?.setImageResource(diceFace)
+                }
+                /* 3 = Orange */
+                3 -> {
+                    diceFace = when (face)
+                    {
+                        1 -> R.drawable.odice_1
+                        2 -> R.drawable.odice_2
+                        3 -> R.drawable.odice_3
+                        4 -> R.drawable.odice_4
+                        5 -> R.drawable.odice_5
+                        6 -> R.drawable.odice_6
+                        else -> R.drawable.odice_6
+                    }
+                    activityBinding.orangeDiceView?.setImageResource(diceFace)
+                }
             }
-            resultImageView.setImageResource(diceFace)
         }
 
-        @SuppressLint("SetTextI18n")
-        fun rollAnimation() {
+        fun setResult(type: Int) {
+            when (type) {
+                0 -> activityBinding.redText.text = diceRollResults[type].toString()
+                1 -> activityBinding.blueText?.text = diceRollResults[type].toString()
+                2 -> activityBinding.purpleText?.text = diceRollResults[type].toString()
+                3 -> activityBinding.orangeText?.text = diceRollResults[type].toString()
+            }
+        }
+        fun rollAnimation(type: Int) {
             activityBinding.rollButton.visibility = View.INVISIBLE
-            resultTextView.text = getString(R.string.rolling_message)
+            activityBinding.redText.text = getString(R.string.rolling_message)
+            activityBinding.blueText?.text = getString(R.string.rolling_message)
+            activityBinding.purpleText?.text = getString(R.string.rolling_message)
+            activityBinding.orangeText?.text = getString(R.string.rolling_message)
             val countDown = object : CountDownTimer(3000, 200) {
                 override fun onTick(p0: Long) {
-                    setDiceFace((1..6).random())
+                    setDiceFace((1..6).random(), type)
                 }
                 override fun onFinish() {
-                    setDiceFace(diceRollResult)
-                    resultTextView.text = diceRollResult.toString()
-                    resultImageView.contentDescription = diceRollResult.toString()
+                    setDiceFace(diceRollResults[type], type)
+                    setResult(type)
+                    //resultTextView.text = diceRollResults[type].toString()
+                    //resultImageView.contentDescription = diceRollResult.toString()
                     activityBinding.rollButton.visibility = View.VISIBLE
                 }
             }
             countDown.start()
         }
-        rollAnimation()
+        rollAnimation(0)
+        rollAnimation(1)
+        rollAnimation(2)
+        rollAnimation(3)
     }
 }
 
